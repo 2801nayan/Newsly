f1cc78474bce9d3eaab9b02ddc0ba9009563d339
2801nayan <nayankalsariya2801@gmail.com>
2022-10-16 19:04:29 +0530

    newsly with class based component code

--- src/components/News.js
+++ src/components/News.js
@@ -37,13 +37,13 @@ export class News extends Component {
     this.setState({
       article: parseData.articles,
       totalResult: parseData.totalResults,
-      page: this.state.page+1,
-      loading: false,
+      page: this.state.page + 1,
+      loading: false
     });
     this.props.setProgress(100);
   }
   async componentDidMount() {
-    this.updateNews();
+    this.updateNews(  );
   }
   fetchMoreData = async () => {
     const dataURL = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&APIKey=${this.props.APIKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -53,18 +53,16 @@ export class News extends Component {
       article: this.state.article.concat(parseData.articles),
       totalResult: parseData.totalResults,
       page: this.state.page + 1,
-      loading: false,
+      loading: false
     });
   };
   
   render() {
     return (
       <>
-        {console.log("total = " + this.state.totalResult)}
         <h1 className="text-center" style={{ margin: "80px 0px 25px 0px" }}>
           Newsly - Top {this.props.category} Headlines
         </h1>
-        {console.log("total fetched = " + this.state.article.length)}
         {this.state.loading && <Spinner />}
         <InfiniteScroll
           dataLength={this.state.article.length}
